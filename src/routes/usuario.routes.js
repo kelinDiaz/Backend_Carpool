5f871abef81db840df7c9373c939be9f133ed8ab
valeriap14 <valepb001@gmail.com>
2025-06-27 15:26:58 -0600

    cambio

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
 
 router.post('/usuarios', usuarioController.registrarUsuario);
-router.post('/usuarios/inisioSesion', usuarioController.inicioSesion);
+router.post('/usuarios/inicioSesion', usuarioController.inicioSesion);
 
 module.exports = router;
