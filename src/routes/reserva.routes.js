8136d6b1eee4150857c93a02e0e261cfd73577ae
Kelin Diaz <kadiazs@unah.hn>
2025-07-22 09:51:09 -0600

    routes de reservar , solicitar , responder , obtener reservas

---
+++ src/routes/reserva.routes.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const reservaController = require('../controllers/reserva.controller');
+
+router.post('/solicitar', reservaController.solicitarReserva);
+
+router.put('/responder/:id', reservaController.responderReserva);
+
+router.get('/conductor/:conductor_id', reservaController.listarReservasConductor);
+router.get('/pasajero/:pasajero_id', reservaController.listarReservasPasajero);
+
+module.exports = router;
